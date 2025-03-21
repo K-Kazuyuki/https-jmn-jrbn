ab18bc0fb6111c678fd784783946031adad05f8c
K-Kazuyuki <kisidakazuyuki@gmail.com>
2025-03-22 00:47:31 +0900

    StartGameページのデザインを更新し、ゲーム名入力フィールドと参加者上限スライダーを追加

--- src/StartGame.tsx
+++ src/StartGame.tsx
@@ -1,7 +1,31 @@
+import { Button, Slider, TextField, Typography } from "@mui/material";
+
 const StartGamePage = () => {
   return (
     <div>
-      <h1>Start Game Page</h1>
+      <Typography variant="h4" sx={{ margin: "40px" }}>
+        ゲームを始める
+      </Typography>
+      <Typography variant="h5">この回の名前</Typography>
+      <TextField
+        label="例: 最高裁交流回"
+        variant="outlined"
+        fullWidth
+        style={{ margin: "20px" }}
+      />
+      <Typography variant="h5">参加者上限</Typography>
+      <Slider
+        defaultValue={4}
+        step={1}
+        marks
+        min={2}
+        max={10}
+        valueLabelDisplay="auto"
+        style={{ margin: "20px" }}
+      />
+      <Button variant="contained" color="primary" style={{ margin: "20px" }}>
+        ゲームを始める
+      </Button>
     </div>
   );
 };
