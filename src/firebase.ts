093146cc165b9a5c4000c392509ccc0042d985ee
K.Kazuyuki <{kisidakazuyuki@gmail.com}>
2025-03-26 17:11:26 +0900

    Firebase設定を環境変数から読み込むように変更し、api_key.jsonを削除。firebaseConfig.tsを新規作成し、設定を整理。

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import api_key from "./api_key.json";
+import firebaseConfig from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,7 +9,7 @@ import api_key from "./api_key.json";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
-const app = initializeApp(api_key);
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { app, auth };
