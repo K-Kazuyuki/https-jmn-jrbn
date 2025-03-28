b697a789cb712ce8a4a07e6b0fd7e5f3d566ffa9
K.Kazuyuki <{kisidakazuyuki@gmail.com}>
2025-03-29 01:19:44 +0900

    Firebase設定をViteの環境変数から読み込むように変更し、エラーチェックを追加。firebaseConfig.tsのエクスポート形式を修正。

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+if (firebaseConfig.apiKey === "" || firebaseConfig.authDomain === "") {
+  throw new Error("環境変数が設定されていません．");
+}
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { app, auth };
+export const auth = getAuth(app); // 名前付きエクスポートに変更
+export default app; // appのみデフォルトエクスポート
