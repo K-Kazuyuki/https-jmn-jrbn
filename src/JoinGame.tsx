13ec9c3a62f8e6d8b063ba4998e50c5ac392ae25
K.Kazuyuki <{kisidakazuyuki@gmail.com}>
2025-03-31 23:50:39 +0900

    参加者名を入力する機能を追加し、セッションID取得後にゲームに参加するボタンを表示するように変更。エラーメッセージの処理を改善。

--- src/JoinGame.tsx
+++ src/JoinGame.tsx
@@ -4,13 +4,14 @@ import axios from "axios";
 const JoinGame: React.FC = () => {
   const [entryWord, setEntryWord] = useState("");
   const [errorMessages, setErrorMessages] = useState("");
+  const [sessionId, setSessionId] = useState("");
+  const [playerName, setPlayerName] = useState("");
   const getSessionId = async () => {
     await axios
       .post("/api/getSessionId", { entryWord: entryWord })
       .then((res) => {
         if (res.data.length > 0) {
-          const sessionId = res.data[0].SessionId;
-          window.location.href = `/game?sessionId=${sessionId}`;
+          setSessionId(res.data[0].SessionId);
         } else {
           setErrorMessages("セッションが見つかりませんでした。");
         }
@@ -18,6 +19,28 @@ const JoinGame: React.FC = () => {
       .catch((error) => {
         console.error("Error fetching session ID:", error);
         setErrorMessages("セッションが見つかりませんでした。");
+      });
+  };
+
+  const JoinGame = async () => {
+    const userId = localStorage.getItem("userId") || "";
+    await axios
+      .post("/api/joinUser", {
+        sessionId: sessionId,
+        playerName: playerName,
+        userId: userId,
+      })
+      .then((res) => {
+        if (res.data.length > 0) {
+          console.log("参加成功", res.data);
+          window.location.href = `/game?sessionId=${sessionId}`;
+        } else {
+          setErrorMessages("参加に失敗しました。");
+        }
+      })
+      .catch((error) => {
+        console.error("Error joining game:", error);
+        setErrorMessages("参加に失敗しました。" + error.message);
       });
   };
 
@@ -34,14 +57,37 @@ const JoinGame: React.FC = () => {
         value={entryWord}
         onChange={(e) => setEntryWord(e.target.value)}
       />
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        onClick={getSessionId}
-      >
-        参加する
-      </Button>
+      {sessionId === "" && (
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={getSessionId}
+        >
+          参加する
+        </Button>
+      )}
+      {sessionId && (
+        <>
+          <Typography variant="h5">プレイヤー名</Typography>
+          <TextField
+            label="例: 柑橘 旨美"
+            variant="outlined"
+            fullWidth
+            style={{ margin: "20px" }}
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={JoinGame}
+          >
+            参加する
+          </Button>
+        </>
+      )}
       {errorMessages && (
         <Typography variant="body2" color="error" style={{ margin: "20px" }}>
           {errorMessages}
